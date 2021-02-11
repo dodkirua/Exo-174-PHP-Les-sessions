966da69cf3ef9b29a18a50315bdd0949309c0212
dodkirua <pybouttefeux@gmail.com>
2021-02-11 09:19:21 +0100

    finis

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
+$_SESSION['couleur']="rouge";
 
+echo "<a href='index2.php' title='page 2'>Page 2</a>";
 // Premiere ligne
 
 
---
+++ index2.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+echo "<p>".$_SESSION['couleur']."</p>";
